cd5381a52c30a4d495ab23455d4b2894fb13ec4e
dinesh.srinivasan <dinesh.srinivasan@N13187.intellectdesign.com>
2021-09-23 11:27:11 +0530

    Second Commit for TestApp

--- TestApp/src/TestMain.java
+++ TestApp/src/TestMain.java
@@ -1,9 +1,17 @@
 
 public class TestMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
 		System.out.println("Hello Git");
+		
+		TestEntity t=new TestEntity();
+		t.setName("Amarnath");
+		System.out.println(t.getName());
+		
+		TestStudent  ts=new TestStudent();
+		ts.setStdName("amar");
 	}
 
 }
---
+++ TestApp/src/TestStudent.java
@@ -0,0 +1,13 @@
+
+public class TestStudent 
+{
+	private String StdName;
+
+	public String getStdName() {
+		return StdName;
+	}
+
+	public void setStdName(String stdName) {
+		StdName = stdName;
+	}
+}
