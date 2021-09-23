77062c7a5ef265f06cfa4ab0c16e5a2885cadfd2
dinesh.srinivasan <dinesh.srinivasan@N13187.intellectdesign.com>
2021-09-23 12:27:22 +0530

    Emp commit

---
+++ Student/src/com/emp/TestEmp.java
@@ -0,0 +1,14 @@
+package com.emp;
+
+public class TestEmp 
+{
+	private String empname;
+
+	public String getEmpname() {
+		return empname;
+	}
+
+	public void setEmpname(String empname) {
+		this.empname = empname;
+	}
+}
