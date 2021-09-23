5b431a49eabb0b6cd0ad7e71d21c1f869af810f4
dinesh.srinivasan <dinesh.srinivasan@N13187.intellectdesign.com>
2021-09-23 11:38:41 +0530

    test second commit

---
+++ Student/src/StdMain.java
@@ -0,0 +1,12 @@
+
+public class StdMain {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		TestStudent ts =new TestStudent();
+		ts.setStdName("amar");
+		System.out.println(ts.getStdName());
+	}
+
+}
