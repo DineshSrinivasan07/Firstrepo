65696500cb6d0d21f496b91932f40ffbb425c0d0
dinesh.srinivasan <dinesh.srinivasan@N13187.intellectdesign.com>
2021-09-23 12:46:22 +0530

    ply commit

---
+++ Student/src/com/player/PlayerMain.java
@@ -0,0 +1,12 @@
+package com.player;
+
+public class PlayerMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TestPlay tp =new TestPlay();
+		tp.setPlyName("amarnath");
+		System.out.println(tp.getPlyName());
+	}
+
+}
